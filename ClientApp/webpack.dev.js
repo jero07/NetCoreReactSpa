668a29040059f6e4da1f1e1690a047de0a709522
jero07 <theres.07@gmail.com>
2018-08-30 16:58:35 -0700

    remove WebpackMd5Hash modules

--- ClientApp/webpack.dev.js
+++ ClientApp/webpack.dev.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackMd5Hash = require('webpack-md5-hash');
+//const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');  
@@ -57,7 +57,7 @@ const config = {
       template: './public/index.html',
       filename: 'index.html'
     }),
-    new WebpackMd5Hash(),
+   // new WebpackMd5Hash(),
     new webpack.HotModuleReplacementPlugin()
   ]
 };
--- ClientApp/webpack.prod.js
+++ ClientApp/webpack.prod.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackMd5Hash = require('webpack-md5-hash');
+//const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
@@ -10,7 +10,7 @@ const config = {
   entry: { main: './src/index.js' },
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: '[name].[chunkhash].js'
+    filename: '[name].[hash].js'
   },
   mode: 'production', 
   optimization: {
@@ -42,15 +42,15 @@ const config = {
   plugins: [
     new CleanWebpackPlugin('build', {}),
     new MiniCssExtractPlugin({
-      filename: 'style.[contenthash].css',
+      filename: 'style.[hash].css',
     }),
     new HtmlWebpackPlugin({
       inject: false,
       hash: true,
       template: './public/index.html',
       filename: 'index.html'
-    }),
-    new WebpackMd5Hash() 
+    })
+   // new WebpackMd5Hash() 
   ]
 };
 
