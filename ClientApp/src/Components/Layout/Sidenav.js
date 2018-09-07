b51fdc4fa9396ac1e551108f32b2764083e8f37a
Jerome <theres.07@gmail.com>
2018-09-06 22:08:34 -0700

    merfe

--- ClientApp/src/Components/Layout/Sidenav.js
+++ ClientApp/src/Components/Layout/Sidenav.js
@@ -1,43 +1,37 @@
 import React, {Component} from 'react';
-import {withStyles, Typography, List, Divider, IconButton} from '@material-ui/core';
-import {Link} from 'react-router-dom';
 
 const styles = {
-  root: {
-    flexGrow: 1
-  },
-  leftnav: {
-    width: 240,
-    backgroundColor: '#fff'
-  },
-  sidelist: {
-    padding: 10
-  }
+    root: {
+      flexGrow: 1
+    },
+    leftnav:{
+      width: 240,
+      backgroundColor: '#fff'
+    },
+    sidelist:{
+      padding:10
+    }
 }
 class SideNav extends Component {
   constructor() {
     super();
   }
   render() {
-
-    const sideList = (
-      <div style={styles.leftnav}>
-        <div style={styles.sidelist}>
-          <List>
-            <Link to='/'>Home</Link>
-          </List>
-          <Divider/>
-          <List>
-            <Link to='/dash'>Dash</Link>
-          </List>
-        </div>
+      
+   const sideList = (
+    <div style={styles.leftnav}>
+      <div style={styles.sidelist}>
+      <List>Test1</List>
+      <Divider />
+      <List>Test2</List>
       </div>
-    );
+    </div>
+  );
 
     return (
-      <React.Fragment>
-        {sideList}
-      </React.Fragment>
+     <React.Fragment>
+       {sideList}
+     </React.Fragment>
     )
   }
 }
